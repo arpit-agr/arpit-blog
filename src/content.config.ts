3ec01582450800ee478bba1448662acec386896a
arpit-agr <arpit.agrawal2502@gmail.com>
2025-08-25 16:41:16 +0530

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -4,7 +4,6 @@ import { glob } from "astro/loaders";
 const notes = defineCollection({
 	loader: glob({ base: "./src/data/notes", pattern: "**/*.{md,mdoc}" }),
 	schema: z.object({
-		title: z.string().optional(),
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 	}),
