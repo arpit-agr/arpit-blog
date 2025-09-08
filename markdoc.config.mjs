348cfa598c38a1f36626a1f495204ddcbeda6c79
arpit-agr <arpit.agrawal2502@gmail.com>
2025-09-08 07:22:37 +0530

    Update markdoc.config.mjs

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -29,7 +29,7 @@ export default defineMarkdocConfig({
 			// Choose from Shiki's built-in themes (or add your own)
 			// Default: 'github-dark'
 			// https://shiki.style/themes
-			theme: "light-plus",
+			theme: "slack-ochin",
 			// Enable word wrap to prevent horizontal scrolling
 			// Default: false
 			wrap: false,
