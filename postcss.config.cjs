8197b2f8d07efc11b9a753b556b9e7e6551099c8
arpit-agr <arpit.agrawal2502@gmail.com>
2025-09-17 16:30:05 +0530

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,10 @@ const postcssPresetEnv = require("postcss-preset-env");
 
 module.exports = {
 	plugins: [
+		require("postcss-utopia")({
+			minWidth: 320, // 20rem
+			maxWidth: 896, // 56rem
+		}),
 		postcssPresetEnv({
 			features: {},
 		}),
