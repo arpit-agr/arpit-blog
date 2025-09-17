1fce73f34d5496f72c367e78f685a0a2dc747de6
arpit-agr <arpit.agrawal2502@gmail.com>
2025-09-17 16:24:07 +0530

    Update markdoc.config.mjs

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -16,7 +16,7 @@ export default defineMarkdocConfig({
 	tags: {
 		"idiomatic-text": {
 			/* The i element represents a span of text in an alternate voice or mood, or otherwise offset from the normal prose in a manner indicating a different quality of text, such as a taxonomic designation, a technical term, an idiomatic phrase from another language, transliteration, a thought, or a ship name in Western texts. */
-			render: component("@components/IdiomaticText.astro"),
+			render: "i",
 			attributes: {
 				lang: { type: String },
 				class: { type: String },
