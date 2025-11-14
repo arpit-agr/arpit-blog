9aeb3c5e593bd731133467317c41429fb666b566
arpit-agr <arpit.agrawal2502@gmail.com>
2025-11-14 19:02:18 +0530

    Refactor back link component

---
+++ src/scripts/back-link.js
@@ -0,0 +1,18 @@
+class BackLink extends HTMLElement {
+  connectedCallback() {
+    // Find the inner <a>
+    const link = this.querySelector("a");
+    if (!link) return; // fail gracefully if none
+
+    link.addEventListener("click", (e) => {
+      const ref = document.referrer;
+      if (ref && new URL(ref).origin === location.origin) {
+        e.preventDefault();
+        history.back();
+      } else {
+        location.href = "/";
+      }
+    });
+  }
+}
+customElements.define("back-link", BackLink);
