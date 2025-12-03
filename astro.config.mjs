c7f0823f255eb79fc6757370428cf959160ed764
arpit-agr <arpit.agrawal2502@gmail.com>
2025-12-03 22:11:21 +0530

    add pagefind and html-minifier-next

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,33 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-import expressiveCode from "astro-expressive-code";
-import { pluginLanguageBadge } from "expressive-code-language-badge";
-import mdx from "@astrojs/mdx";
+import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import { pluginLanguageBadge } from 'expressive-code-language-badge';
+import mdx from '@astrojs/mdx';
+import pagefind from 'astro-pagefind';
+import htmlMinifierNext from 'astro-html-minifier-next';
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://arpit.blog",
+	site: 'https://arpit.blog',
 	integrations: [
 		expressiveCode({
 			plugins: [pluginLanguageBadge()],
 		}),
 		mdx(),
+		pagefind(),
+		htmlMinifierNext({
+			caseSensitive: true,
+			collapseBooleanAttributes: true,
+			collapseWhitespace: true,
+			continueOnMinifyError: false,
+			decodeEntities: true,
+			keepClosingSlash: false,
+			preventAttributesEscaping: true,
+			removeComments: true,
+			removeScriptTypeAttributes: true,
+			removeStyleLinkTypeAttributes: true,
+			useShortDoctype: true,
+		}),
 	],
-	trailingSlash: "always",
+	trailingSlash: 'always',
 });
