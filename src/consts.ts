23944c9a78e98b52a79851df1c723f908f809d9c
arpit-agr <arpit.agrawal2502@gmail.com>
2025-12-04 15:31:54 +0530

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,22 +1,26 @@
 export const SITE_TITLE = "Arpit's Blog";
 export const SITE_DESCRIPTION =
-	"The online home of Arpit Agrawal, a web designer-developer from India.";
+	'The online home of Arpit Agrawal, a web designer-developer from India.';
 
 export const NOTES_TITLE = "Notes | Arpit's Blog";
 export const NOTES_DESCRIPTION =
-	"Brief notes from Arpit Agrawal on everything he is reading, learning and observing.";
+	'Brief notes from Arpit Agrawal on everything he is reading, learning and observing.';
 
 export const ARTICLES_TITLE = "Articles | Arpit's Blog";
 export const ARTICLES_DESCRIPTION =
-	"Long form articles from Arpit Agrawal, a web designer-developer from India.";
+	'Long form articles from Arpit Agrawal, a web designer-developer from India.';
 
 export const LINKS_TITLE = "Links | Arpit's Blog";
 export const LINKS_DESCRIPTION =
-	"Hand-picked links from the depths of the interweb by Arpit Agrawal.";
+	'Hand-picked links from the depths of the interweb by Arpit Agrawal.';
 
 export const LIBRARY_TITLE = "Library | Arpit's Blog";
-export const LIBRARY_DESCRIPTION = "Books owned and read by Arpit Agrawal.";
+export const LIBRARY_DESCRIPTION = 'Books owned and read by Arpit Agrawal.';
 
 export const ANTILIBRARY_TITLE = "Antilibrary | Arpit's Blog";
 export const ANTILIBRARY_DESCRIPTION =
-	"Books owned but not yet read by Arpit Agrawal.";
+	'Books owned but not yet read by Arpit Agrawal.';
+
+export const SEARCH_TITLE = "Search | Arpit's Blog";
+export const SEARCH_DESCRIPTION =
+	'Search across notes, articles and links published by Arpit Agrawal.';
