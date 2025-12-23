9162b63e35d68088d53918497036b5a0c9cd9c7b
arpit-agr <arpit.agrawal2502@gmail.com>
2025-12-23 17:11:33 +0530

    add book count and logic to update it

--- src/scripts/book-filters.js
+++ src/scripts/book-filters.js
@@ -1,25 +1,35 @@
-const fieldset = document.querySelector("fieldset");
-const inputs = fieldset.querySelectorAll("input");
-const books = Array.from(document.querySelectorAll(".book-list li"));
+const fieldset = document.querySelector('fieldset');
+const inputs = fieldset.querySelectorAll('input');
+const books = Array.from(document.querySelectorAll('.book-list li'));
+let bookResultsCount = document.getElementById('books-results-count');
 
 function applyFilter(value) {
 	books.forEach((item) => {
-		const matches = value === "all" || item.dataset.tags.includes(value);
-		item.toggleAttribute("hidden", !matches);
+		const matches = value === 'all' || item.dataset.tags.includes(value);
+		item.toggleAttribute('hidden', !matches);
 	});
 }
 
-fieldset.removeAttribute("disabled");
+function updateCounts() {
+	const count = books.filter((book) => !book.hasAttribute('hidden')).length;
+	bookResultsCount.textContent = `${count} ${count === 1 ? 'book' : 'books'}`;
+}
+
+fieldset.removeAttribute('disabled');
 
 inputs.forEach((input) => {
-	input.addEventListener("change", (e) => {
+	input.addEventListener('change', (e) => {
 		const value = e.target.value;
 
 		if (!document.startViewTransition) {
 			applyFilter(value);
+			updateCounts();
 			return;
 		}
 
-		document.startViewTransition(() => applyFilter(value));
+		document.startViewTransition(() => {
+			applyFilter(value);
+			updateCounts();
+		});
 	});
 });
