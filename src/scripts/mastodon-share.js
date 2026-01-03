1af9a95c60966ccc98429a1c1bcfb250f19e32da
arpit-agr <arpit.agrawal2502@gmail.com>
2026-01-03 11:54:51 +0530

    Update mastodon-share.js

--- src/scripts/mastodon-share.js
+++ src/scripts/mastodon-share.js
@@ -1,9 +1,9 @@
 // I got the key, I got the secret…
-let key = "mastodon-instance";
+let key = 'mastodon-instance';
 let instance = localStorage.getItem(key);
 
 // get the link from the DOM
-const button = document.querySelector(".mastodon-share");
+const button = document.querySelector('.mastodon-share');
 
 // refresh the link with the instance name
 const refreshlink = (instance) => {
@@ -13,44 +13,44 @@ const refreshlink = (instance) => {
 // got it? Let's go!
 if (button) {
 	// labels and texts from the link
-	let prompt = button.dataset.prompt || "Please tell me your Mastodon instance";
-	let editlabel = button.dataset.editlabel || "Edit your Mastodon instance";
-	let edittext = button.dataset.edittext || "✏️";
+	let prompt = button.dataset.prompt || 'Please tell me your Mastodon instance';
+	let editlabel = button.dataset.editlabel || 'Edit your Mastodon instance';
+	let edittext = button.dataset.edittext || '✏️';
 
 	// Ask the user for the instance name and set it…
 	const setinstance = (_) => {
 		instance = window.prompt(prompt, instance);
 		if (instance) {
 			localStorage.setItem(key, instance);
-			createeditbutton();
+			createEditButton();
 			refreshlink(instance);
 			button.click();
 		}
 	};
 
 	// create and insert the edit link
-	const createeditbutton = (_) => {
-		if (document.querySelector("button.mastodon-edit")) return;
-		let editlink = document.createElement("button");
+	const createEditButton = (_) => {
+		if (document.querySelector('button.mastodon-edit')) return;
+		let editlink = document.createElement('button');
 		editlink.innerText = edittext;
-		editlink.classList.add("mastodon-edit", "text-box-trim");
+		editlink.classList.add('mastodon-edit', 'text-box-trim');
 		editlink.title = editlabel;
 		editlink.ariaLabel = editlabel;
-		editlink.addEventListener("click", (e) => {
+		editlink.addEventListener('click', (e) => {
 			e.preventDefault();
 			localStorage.removeItem(key);
 			setinstance();
 		});
-		button.insertAdjacentElement("afterend", editlink);
+		button.insertAdjacentElement('afterend', editlink);
 	};
 
 	// if there is  a value in localstorage, create the edit link
 	if (localStorage.getItem(key)) {
-		createeditbutton();
+		createEditButton();
 	}
 
 	// When a user clicks the link
-	button.addEventListener("click", (e) => {
+	button.addEventListener('click', (e) => {
 		// If the user has already entered their instance
 		// and it is in localstorage write out the link href
 		// with the instance and the current page title and URL
